9127bb76faf93cdf1492aecc56cf8985a1d2cd31
Diego Sperber <diegosperber@Diegos-MacBook-Pro.local>
2024-08-22 11:02:29 +0200

    primer commit

---
+++ src/main/java/com/emarsu/jazztock/JazztockApplication.java
@@ -0,0 +1,13 @@
+package com.emarsu.jazztock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazztockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazztockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emarsu/jazztock/JazztockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emarsu.jazztock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazztockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
