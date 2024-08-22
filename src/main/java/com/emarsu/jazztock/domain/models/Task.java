e21249f19d4c07620211c690ed78c892134ab973
Diego Sperber <diegosperber@Diegos-MacBook-Pro.local>
2024-08-22 12:10:58 +0200

    estructura de carpetas

---
+++ src/main/java/com/emarsu/jazztock/domain/models/Task.java
@@ -0,0 +1,17 @@
+package com.emarsu.jazztock.domain.models;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime creationDate;
+
+    private boolean completed;
+
+}
